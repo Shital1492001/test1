d1dde00bb49f06cc1a25e330bfb060b02b807909
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-05-22 20:38:26 +0530

    annotate rating controller by restcontroller

--- src/main/java/com/zosh/controller/RatingController.java
+++ src/main/java/com/zosh/controller/RatingController.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.zosh.exception.UserException;
 import com.zosh.modal.Rating;
@@ -16,6 +19,8 @@ import com.zosh.modal.User;
 import com.zosh.service.RatingServices;
 import com.zosh.service.UserService;
 
+@RestController
+@RequestMapping("/api/ratings")
 public class RatingController {
 	
 	private UserService userService;
@@ -27,6 +32,7 @@ public class RatingController {
 		// TODO Auto-generated constructor stub
 	}
 
+	@PostMapping("/create")
 	public ResponseEntity<Rating> createRatingHandler(@RequestBody Rating req,@RequestHeader("Authorization") String jwt) throws UserException{
 		User user=userService.findUserProfileByJwt(jwt);
 		Rating rating=ratingServices.createRating(req, user);
--- src/main/java/com/zosh/controller/ReviewController.java
+++ src/main/java/com/zosh/controller/ReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,7 @@ public class ReviewController {
 		this.userService=userService;
 		// TODO Auto-generated constructor stub
 	}
-	
+	@PostMapping("/create")
 	public ResponseEntity<Review> createReviewHandler(@RequestBody Review req,@RequestHeader("Authorization") String jwt) throws UserException{
 		User user=userService.findUserProfileByJwt(jwt);
 		Review review=reviewService.createReview(req, user);
